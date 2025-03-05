b5aab92b1950ee0481e1c9533f0661954c23c841
callmetandat <letandat15082002@gmail.com>
2025-03-05 23:53:22 +0700

    SET25-149 | Super basic JavaScript

---
+++ Homework Day 7/Ex1.js
@@ -0,0 +1,9 @@
+let mathScore = 8;
+let physicsScore = 7;
+let chemistryScore = 9;
+
+// Calculate average score
+let averageScore = (mathScore + physicsScore + chemistryScore) / 3;
+
+// Print result
+console.log("Average score:", averageScore);

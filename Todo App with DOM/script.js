e87b1f96a1b1d13eb72a23a5f665aef824807a46
callmetandat <letandat15082002@gmail.com>
2025-06-30 18:41:02 +0700

    Update renderTask, statusSelect function

--- Todo App with DOM/script.js
+++ Todo App with DOM/script.js
@@ -1,64 +1,140 @@
-function createContainer(type, className, id="") {
-    const container = document.createElement(type);
-    container.className = className;
+function createContainer(id="") {
+    const container = document.createElement("div");
+    container.className = "task-item";
     container.id = id;
     return container;
 }
 
-var flag = true;
-
-function validateForm() {
-    let x = document.forms["task-form"]["fname"].value;
-    if (x == "") {
-        flag = false;
-        alert("Name must be filled out");
-        return false;
-    }
+function createEditButton() {
+    const editButton = document.createElement("button");
+    editButton.textContent = "EDIT";
+    editButton.className = "todo__button todo__button--edit";
+    return editButton;
 }
 
-let count = 0;
+function createDeleteButton() {
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "DELETE";
+    deleteButton.className = "todo__button todo__button--delete";
+    return deleteButton;
+}
 
-window.onload = function() {
-    const addTaskButton = document.getElementById("add-task-button");
-    const taskList = document.getElementById("task-list");
+function createStateSelect(currentStatus="To-do") {
+    const select = document.createElement("select");
+    select.className = "todo__select";
+    const options = ["To-do", "Pending", "In-progress", "Completed"];
+    options.forEach(option => { 
+        const status = document.createElement("option");
+        status.value = option;
+        status.textContent = option.charAt(0).toUpperCase() + option.slice(1);
+        
+        if (option === currentStatus) {
+            status.selected = true;
+        }
+        select.appendChild(status);
+    });
+    return select;
+}
+
+function handleDeleteButton(deleteButton) {
+    deleteButton.addEventListener("click", () => {
+        const task = deleteButton.parentElement;
+        task.remove();
+    });
+}
+
+function handleEditButton(editButton) {
+    editButton.addEventListener("click", () => {
+        const task = editButton.parentElement;
+        if (editButton.textContent === "EDIT") {
+            const taskSpan = task.querySelector("span");
     
-    addTaskButton.addEventListener("click", () => {
-        if (flag === true) {
-            console.log(flag);
-            // Create div container
-            let container = createContainer("div", "item", `item${count}`);
-    
-            // Get task name
-            const taskName = document.getElementById("task-input").value;
-    
-            const task = document.createElement("span");
-            task.textContent = taskName;
-            
-            // Create edit task button
-            const editButton = document.createElement("button");
-            editButton.id = `edit-button-${count}`;
+            const inputTask = document.createElement("input");
+            inputTask.type = "text";
+            inputTask.className = "todo__edit-input";
+            task.replaceChild(inputTask, taskSpan);
+            editButton.textContent = "DONE";
+            inputTask.focus();
+        } else {
+            const inputTask = task.querySelector("input");
+            const newTaskName = inputTask.value.trim();
+            if (newTaskName === "") {
+                alert("Task cannot be empty");
+                return;
+            }
+            const newTask = document.createElement("span");
+            newTask.textContent = newTaskName;
+            task.replaceChild(newTask, inputTask);
             editButton.textContent = "EDIT";
-            editButton.className = "todo__button todo__button--edit"
-            
-            // Create delete task button
-            const deleteButton = document.createElement("button")
-            deleteButton.id = `delete-button-${count}`;
-            deleteButton.textContent = "DELETE";
-            editButton.className = "todo__button todo__button--delete"
-            
-            deleteButton.addEventListener("click", () => {
-                const task = document.getElementById(`item${deleteButton.id.split('-')[2]}`)
-                task.remove();
-            })
-    
-            // Append to div container
-            container.appendChild(task);
-            container.appendChild(editButton);
-            container.appendChild(deleteButton)
-    
-            // Append container to list
-            taskList.appendChild(container);
-            count++;
         }
     });
 }
+
+function handleStatusSelect(statusSelect) {
+        statusSelect.addEventListener("change", () => {
+        const task = statusSelect.parentElement;
+        let option = statusSelect.options[statusSelect.selectedIndex].text;
+        option = option.charAt(0).toLowerCase() + option.slice(1);
+        task.classList.remove("task-item--pending", "task-item--completed", "task-item--in-progress");
+        task.className += ` task-item--${option}`;
+    })
+}
+
+function renderTask(taskList, taskName, status="to-do") {
+    const taskContainer = createContainer(`${Date.now()}`);
+    const taskSpan = document.createElement("span");
+    taskSpan.textContent = taskName;
+
+    const editButton = createEditButton();
+    handleEditButton(editButton);
+
+    const deleteButton = createDeleteButton();
+    handleDeleteButton(deleteButton);
+
+    const statusSelect = createStateSelect(status);
+    handleStatusSelect(statusSelect);
+
+    taskContainer.classList.add(`task-item--${status.charAt(0).toLowerCase() + status.slice(1)}`);
+    taskContainer.appendChild(statusSelect);
+    taskContainer.appendChild(taskSpan);
+    taskContainer.appendChild(editButton);
+    taskContainer.appendChild(deleteButton);
+    taskList.appendChild(taskContainer);
+}
+
+function savedTask(id, taskName, status="to-do") {
+    taskObject[id] = id;
+    taskObject[taskName] = taskName;
+    taskObject[status] = status;
+}
+
+let taskObject = {};
+
+window.onload = function() {
+    const addTaskButton = document.getElementById("add-task-button");
+    const taskForm = document.getElementById("task-form");
+    const taskList = document.getElementById("task-list");
+
+    // Load tasks from localStorage
+    // Render tasks
+
+    taskForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        const input = document.getElementById("task-input");
+        const taskName = input.value.trim();
+
+        if (taskName === "") {
+            alert("Please enter a task!");
+            return;
+        }
+
+        renderTask(taskList, taskName, "In-progress");
+
+        // Save task into localStorage
+        taskOject[taskName] = 
+
+        // Append container to list
+        input.value = ""; // Reset input
+    });
+};

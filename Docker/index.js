b6715f799a9a73115bce97777a733fa19adfe94a
callmetandat <letandat15082002@gmail.com>
2025-07-07 17:02:14 +0700

    Set up CI/CD pipeline

--- Docker/index.js
+++ Docker/index.js
@@ -1,1 +1,10 @@
-console.log("Hi 3000");
+const http = require('http');
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.end('Hello from Node.js 20 + Docker!');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
